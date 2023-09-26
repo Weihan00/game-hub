b575415a7dc7939f0891e0ca275fbcce5002fbaa
wpang <pangweihan12@gmail.com>
2023-09-26 22:34:59 +0200

    building game card

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,16 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+  }
+  
+  interface FetchGameData {
+    count: number;
+    results: Game[];
+  }
 const useGames = () => {
-    interface Game {
-        id: number;
-        name: string;
-      }
-      
-      interface FetchGameData {
-        count: number;
-        results: Game[];
-      }
+
 
       const [games, setGames] = useState<Game[]>([]);
       const [error, setError] = useState("");
