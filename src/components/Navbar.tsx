9857890675c1df97907a5da6a70c47a98e41cfe0
wpang <pangweihan12@gmail.com>
2023-10-11 14:12:08 +0200

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,17 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 // interface Props {
 //   onSearch: (searchText: string) => void;
 // }
 const Navbar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize={16} />
+      <Link to="/">
+        <Image src={logo} boxSize={16} objectFit="cover" />
+      </Link>
       <SearchInput></SearchInput>
       <ColorModeSwitch />
     </HStack>
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGameDetail from "../hooks/useGameDetail";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
@@ -13,13 +13,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <div>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id}></GameTrailer>
-      <GameScreenshot gameId={game.id}></GameScreenshot>
-    </div>
+    <SimpleGrid columns={{ base: 1, md: 2 }}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id}></GameTrailer>
+        <GameScreenshot gameId={game.id}></GameScreenshot>
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
